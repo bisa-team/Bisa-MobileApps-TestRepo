bb55de60aab6cfaa2b839f174a4423848d9e15ff
Oktovivian <vianokto30@gmail.com>
2023-03-03 21:35:54 +0700

    update login 2

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,175 +9,184 @@ class login extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Stack(
+        body: ListView(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Stack(
               children: [
-                Image(
-                  image: AssetImage("assets/img/Image-background-1.png"),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image(
+                      image: AssetImage("assets/img/Image-background-2.png"),
+                    ),
+                    SizedBox(
+                      height: 280,
+                    ),
+                    Image(
+                      image: AssetImage("assets/img/Image-background-1.png"),
+                    ),
+                  ],
                 ),
-                Image(
-                  image: AssetImage("assets/img/Image-background-2.png"),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: width,
-              height: height,
-              child: Image(
-                image: AssetImage(
-                  "assets/img/background-color.png",
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(
-              width: width,
-              height: height,
-              child: Image(
-                image: AssetImage(
-                  "assets/img/background.png",
-                ),
-                fit: BoxFit.fill,
-              ),
-            ),
-            Column(
-              children: [
                 SizedBox(
-                  height: 180,
-                ),
-                Center(
+                  width: width,
+                  height: height,
                   child: Image(
-                    image: AssetImage("assets/img/Logo.png"),
+                    image: AssetImage(
+                      "assets/img/background-color.png",
+                    ),
+                    fit: BoxFit.cover,
                   ),
                 ),
                 SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Masuk Akun",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ],
+                  width: width,
+                  height: 720,
+                  child: Image(
+                    image: AssetImage(
+                      "assets/img/background.png",
+                    ),
+                    fit: BoxFit.fitWidth,
                   ),
                 ),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                Center(
-                  child: SizedBox(
-                    width: width * 0.8,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        labelText: 'Email',
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 180,
+                    ),
+                    Center(
+                      child: Image(
+                        image: AssetImage("assets/img/Logo.png"),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                Center(
-                  child: SizedBox(
-                    width: width * 0.8,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        labelText: 'Password',
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Masuk Akun",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SizedBox(
-                        width: width * 0.05,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Lupa Password?',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            decoration: TextDecoration.underline,
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: width * 0.8,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.person),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: SizedBox(
-                    width: width * 0.8,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0XFF25DB91),
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
+                    ),
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: width * 0.8,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 15),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              labelText: 'Password',
+                              prefixIcon: Icon(Icons.lock)),
                         ),
-                      ),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white, fontSize: 15),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Belum punya akun?",
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 35),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            width: width * 0.05,
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Lupa Password?',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Daftar Sekarang",
-                          style: TextStyle(
-                            color: Color(0XFF196276),
-                            decoration: TextDecoration.underline,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: width * 0.8,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0XFF25DB91),
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 35),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Belum punya akun?",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Daftar Sekarang",
+                              style: TextStyle(
+                                color: Color(0XFF196276),
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ],
-            ),
+            )
           ],
         ),
       ),
