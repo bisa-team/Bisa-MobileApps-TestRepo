53b5bd32654c43e9a9474caa1b5da33459120b14
aldianprawira <aldianprawira99@gmail.com>
2023-03-04 09:22:41 +0700

    blabla

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double = "v";
+
     return const MaterialApp(
       home: SplashScreen(),
     );
